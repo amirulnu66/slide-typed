9460a0ddde102a6e7447f3b2f6888086b230a1f3
amirulnu66 <amirulbd.it@gmail.com>
2017-04-26 20:47:53 +0600

    main slider typed

---
+++ scripts/jss.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+      Typed.new('#head-text', {
+        strings: ["Sign in to ThemeForest", " Envato Account"],
+        typeSpeed: 0.7,
+        loop:true
+        
+      });
+ 
+});
